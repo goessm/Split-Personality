f1b17c3068afe1f797387e2a4f325049a5d4c36f
Damian <damian.swientek@gmx.de>
2022-11-05 16:01:08 +0100

    inv script

--- Assets/Damian/Scenes/Assets/Inventory.cs
+++ Assets/Damian/Scenes/Assets/Inventory.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO when item picked up call EventManager.TriggerEvent("zeitungPickUp");
+
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        makeItemInvisible(0);
+        makeItemInvisible(1);
+        makeItemInvisible(2);
+        makeItemInvisible(3);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        EventManager.StartListening("zeitungPickUp", makeItem0Visible);
+        EventManager.StartListening("teddyPickUp", makeItem1Visible);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("zeitungPickUp", makeItem0Visible);
+        EventManager.StopListening("teddyPickUp", makeItem1Visible);
+    }
+
+    public static void makeItem0Visible() {
+        makeItemVisible(0);
+    }
+
+    public static void makeItem1Visible() {
+        makeItemVisible(1);
     }
 
     public static void makeItemVisible(int index)
@@ -21,7 +39,15 @@ public class Inventory : MonoBehaviour
         GameObject invPanel = GameObject.Find("InvPanel");
         GameObject itemSlot = invPanel.transform.GetChild(index).gameObject;
         GameObject item = itemSlot.transform.GetChild(0).gameObject;
-        // make invisible
-        item.transform.localScale = new Vector3(0,0,0);
+        item.SetActive(true);
+    }
+
+    public static void makeItemInvisible(int index)
+    {
+        GameObject invPanel = GameObject.Find("InvPanel");
+        GameObject itemSlot = invPanel.transform.GetChild(index).gameObject;
+        GameObject item = itemSlot.transform.GetChild(0).gameObject;
+        item.SetActive(false);
+        print("HEELLEO");
     }
 }
