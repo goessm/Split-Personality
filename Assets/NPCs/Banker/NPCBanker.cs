ff83e20db47ba434e2a2ab37173edb15775423d7
goessm <44338544+goessm@users.noreply.github.com>
2022-11-05 15:00:10 +0100

    add other npcs

---
+++ Assets/NPCs/Banker/NPCBanker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class NPCBanker : NPC
+{
+
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) {
+            print("hihi");
+            talkToNpc();
+        }
+    }
+}
