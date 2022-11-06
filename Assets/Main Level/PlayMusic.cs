3296b4d5b31c8fb6b6aeaf01de38883704f843b1
goessm <44338544+goessm@users.noreply.github.com>
2022-11-06 15:57:53 +0100

    move paper stand

--- Assets/Main Level/PlayMusic.cs
+++ Assets/Main Level/PlayMusic.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class MainLevel : MonoBehaviour
+public class PlayMusic : MonoBehaviour
 {
     public AudioClip[] Music;
 
