cb0c4aa07594dcf605cf16f814d839af5fa51ce1
goessm <44338544+goessm@users.noreply.github.com>
2022-11-07 15:00:19 +0100

    fix: improve aspect ratio scaling

---
+++ Assets/Main Level/KeepAspectRatio.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+[ExecuteInEditMode]
+public class KeepAspectRatio : MonoBehaviour
+{
+    void Update() {
+        float targetaspect = 16.0f / 9.0f;
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+        float scaleheight = windowaspect / targetaspect;
+        Camera camera = GetComponent<Camera>();
+
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            camera.rect = rect;
+        } else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+            Rect rect = camera.rect;
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+    }
+
+}
